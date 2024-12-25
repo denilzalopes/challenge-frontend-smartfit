c2ca56e22b677400a22e357ec1e4f4550ad5ac7e
Denilza <122271712+denilzalopes@users.noreply.github.com>
2024-12-25 20:37:15 +0100

    ajouts des styles et configurations des routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { Location } from './types/location.interface';
+import { GetUnitsService } from './services/get-units.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'desafio-smartfit';
+  showList = new BehaviorSubject(false);
+  unitsList: Location[] = [];
+
+  constructor(private unitService: GetUnitsService){ }
+
+  onSubmit(){
+    this.unitsList = this.unitService.getFilteredUnits();
+    this.showList.next(true);
+  }
 }
